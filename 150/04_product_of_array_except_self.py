f203fff5a5161981df2f7bd7e72d2df1459b324a
KaranVikas <karanvikas15@gmail.com>
2025-10-21 00:43:32 -0400

    Add solutions for "Contains Duplicate" and "Product of Array Except Self"
    Implemented two algorithms as part of problem-solving practice. The first checks for duplicates in an array using a hashset, and the second computes the product of all elements except the current one without using division.

---
+++ 150/04_product_of_array_except_self.py
@@ -0,0 +1,30 @@
+class Solution:
+    def productExceptSelf(selfself, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+            print(i, res[i], postfix)
+
+# nums = [1,2,3,4]
+# res = [1]*(len(nums))
+# print(res)
+# prefix = 1
+# for i in range(len(nums)):
+#     res[i] = prefix
+#     prefix *= nums[i]
+#     print(i, res[i], prefix)
+# postfix = 1
+# print(res)
+# for i in range(len(nums)-1,-1,-1):
+#     res[i] *= postfix
+#     postfix *= nums[i]
+#     print(i, res[i], postfix)
+# # return res
+# print(res)
